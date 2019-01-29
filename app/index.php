b0c0b0fd355e46db802fd8a8062d4b38709c2020
Marco <marco@delight.im>
2019-01-29 14:22:35 +0100

    Comment out handling of routes in closures and document as alternative

--- app/index.php
+++ app/index.php
@@ -1,32 +1,34 @@
 <?php
 
-use Delight\Foundation\App;
-
-$app->get('/', function (App $app) {
-	// do something
-	// ...
-
-	// and return a view
-	echo $app->view('welcome.html.twig', [
-		'users' => [ 'Alice', 'Bob' ]
-	]);
-});
-
-$app->get('/greet/:name', function (App $app, $name) {
-	// do something
-	// ...
-
-	// and return a view
-	echo $app->view('greeting.html.twig', [
-		'name' => $name,
-		'greetingId' => $app->input()->get('greetingId', TYPE_INT)
-	]);
-});
-
-$app->post('/photos/:id/delete', function (App $app, $id) {
-	// do something
-	// ...
-});
+// // OR USE CLOSURES (ANONYMOUS FUNCTIONS) DIRECTLY IN THIS FILE
+//
+// use Delight\Foundation\App;
+//
+// $app->get('/', function (App $app) {
+// 	// do something
+// 	// ...
+//
+// 	// and return a view
+// 	echo $app->view('welcome.html.twig', [
+// 		'users' => [ 'Alice', 'Bob' ]
+// 	]);
+// });
+//
+// $app->get('/greet/:name', function (App $app, $name) {
+// 	// do something
+// 	// ...
+//
+// 	// and return a view
+// 	echo $app->view('greeting.html.twig', [
+// 		'name' => $name,
+// 		'greetingId' => $app->input()->get('greetingId', TYPE_INT)
+// 	]);
+// });
+//
+// $app->post('/photos/:id/delete', function (App $app, $id) {
+// 	// do something
+// 	// ...
+// });
 
 // return an error page for undefined pages
 $app->setStatus(404);
